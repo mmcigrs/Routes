1ee1b8e0b0f81e328533e8a44d989fb280e733a1
m-igarashi <107655755+mmcigrs@users.noreply.github.com>
2022-07-19 02:35:49 +0000

    PHP/laravel08 kadai

--- web.php
+++ web.php
@@ -20,6 +20,9 @@ Route::group(['prefix' => 'admin'], function(){
     //
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 //「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
